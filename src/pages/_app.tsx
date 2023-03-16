1b4b1cbf350ec42d64fc72996bfcffadca79c924
Bogdan21o312 <lutskiv2010@gmail.com>
2023-03-16 21:30:37 +0200

    test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import type {AppProps} from 'next/app'
 import {Provider} from "react-redux";
 import {setupStore} from "@/store/store";
 import {LayoutMain} from "@/layouts/LayoutMain";
+import {Header} from "@/layouts/LayoutMain/Hedaer";
 
 const store = setupStore()
 
@@ -10,9 +11,8 @@ const store = setupStore()
 export default function App({Component, pageProps}: AppProps) {
     return (
         <Provider store={store}>
-            <LayoutMain>
-                <Component {...pageProps} />
-            </LayoutMain>
+            <Header/>
+            <Component {...pageProps} />
         </Provider>
     )
 }
