3feb958d7eba52c0b25a89f6d3b39d53686d6c06
Bogdan21o312 <lutskiv2010@gmail.com>
2023-03-16 13:41:20 +0200

    Updated

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,7 +5,7 @@ export default function index() {
     return (
         <div style={{paddingTop: '400px'}}>
             <MainBlock/>
-            <Slider title={'Title'} link={'/'} linkText={'побачити ще'} action={false}/>
+            <Slider title={'Title1'} link={'/'} linkText={'побачити ще'} action={false}/>
             <Slider title={'Title2'} link={'/'} linkText={'побачити ще'} action={false}/>
         </div>
     );
