fa558bf9c956d159ae959d2bac01885e66b984a9
Bogdan21o312 <lutskiv2010@gmail.com>
2023-03-17 10:17:14 +0200

    test

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,11 +2,15 @@ import {useAppDispatch, useAppSelector} from "@/hooks/redux";
 import {useHover} from "@/hooks/useHover";
 import {useStarRating} from "@/hooks/useStarRating";
 import {useTabsToggle} from "@/hooks/useTabToggle";
+import {useMediaQuery} from "@/hooks/useMediaQuery";
+import {useMobileDetect} from "@/hooks/useMobileDetect";
 
 export {
     useTabsToggle,
     useAppSelector,
     useStarRating,
     useAppDispatch,
-    useHover
+    useHover,
+    useMobileDetect,
+    useMediaQuery
 }
--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,9 +1,15 @@
 import {MainBlock, Slider} from "@/screens/Home/modules";
+import {useMobileDetect} from "@/hooks";
 
 export const Home = () => {
+    const detectMobile = useMobileDetect();
     return (
         <div style={{paddingTop: '400px'}}>
             <MainBlock/>
+            is Mobile: { detectMobile.isMobile() } <br/>
+            is Desktop: { detectMobile.isDesktop() } <br/>
+            is Android: { detectMobile.isAndroid() } <br/>
+            is iOS: { detectMobile.isIos() }
             <Slider title={'Title1'} link={'/'} linkText={'побачити ще'} action={false}/>
             <Slider title={'Title2'} link={'/'} linkText={'побачити ще'} action={false}/>
         </div>
